0f86955e26b17dcaeceda04deba4b7ca48d39b89
ttom921 <ttom921@hotmail.com>
2019-04-17 11:13:07 +0800

    學習flask-restful請求參數處理

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from resources.user import User
+from resources.user import User,Users
+
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(User, '/user/<string:name>')
-
+api.add_resource(Users, '/users/')
 if __name__ == "__main__":
     app.run()
--- resources/user.py
+++ resources/user.py
@@ -1,12 +1,16 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 users = [
-    {'name': 'kitty'}, 
+    {'name': 'kitty'},
     {'name': 'tom'}
 ]
 
 
 class User(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('email', required=True, help='Email is required')
+    parser.add_argument('password', required=True, help='Password is required')
+
     def get(self, name):
         find = [item for item in users if item['name'] == name]
         if len(find) == 0:
@@ -24,14 +28,44 @@ class User(Resource):
         }
 
     def post(self, name):
-        pass
-
+        arg = self.parser.parse_args()
+        user = {
+            'name': name,
+            'email': arg['email'],
+            'password': arg['password']
+        }
+        global users
+        users.append(user)
+        return {
+            'message':'Inser user success',
+            'user':user
+        }
     def put(self, name):
-        pass
+        arg =self.parser.parse_args()
+        find = [item for item in users if item['name']==name]
+        if len(find) == 0:
+            return{
+                'message':'username not exist'
+            },403
+        user = find[0]   
+        user['email']= arg['email'] 
+        user['password'] = arg['password']
+        return {
+            'message':'Update user success',
+            'user':user
+        }
 
     def delete(self, name):
         global users
         users = [item for item in users if item['name'] != name]
         return{
             'message': 'Delete done!'
+        }
+
+
+class Users(Resource):
+    def get(self):
+        return {
+            'message': '',
+            'users': users
         }
